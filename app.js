eea8ec42b3797bffec95531883a3718df483624c
Jacob Beeson <beeson.jp@gmail.com>
2020-11-23 09:02:03 -0500

    Finalized app.js. HTML can be rendered properly.

--- app.js
+++ app.js
@@ -10,53 +10,45 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-const teamDirectory = [];
-
 const questions = [
     {
         type: "input",
-        name: "Name",
+        name: "name",
         message: "What is the employee's name?",
     },
     {
         type: "input",
-        name: "Email",
+        name: "email",
         message: "What is the employee's email?"
     },
     {
         type: "input",
-        name: "Id",
+        name: "id",
         message: "Please assign the employee an ID #:"
     },
     {
         type: "list",
-        name: "Role",
+        name: "role",
         message: "What is the employee's role in your team?",
         choices: ["Manager", "Engineer", "Intern"]
     },
     {
         type: "input",
-        name: "OfficeNumber",
+        name: "officeNumber",
         message: "What is the manager's office number?",
-        when: (answers) => answers.Role === "Manager"
+        when: (answers) => answers.role === "Manager"
     },
     {
         type: "input",
-        name: "engineerGithub",
+        name: "github",
         message: "What is the engineer's GitHub username?",
-        when: (answers) => answers.Role === "Engineer"
+        when: (answers) => answers.role === "Engineer"
     },
-    // {
-    //     type: "input",
-    //     name: "engineerGitHubLink",
-    //     message: "What is the engineer's GitHub profile link?",
-    //     when: (answers) => answers.employeeRole === "Engineer"
-    // },
     {
         type: "input",
-        name: "School",
+        name: "school",
         message: "What school does the Intern attend?",
-        when: (answers) => answers.Role === "Intern"
+        when: (answers) => answers.role === "Intern"
     },
     {
         type: "confirm",
@@ -71,43 +63,49 @@ function initialize() {
     stepOne();
 };
 
+const teamDirectory = [];
+
 function stepOne() {
     inquirer.prompt(questions).then(data => {
-        console.log(data);
-        if (data.employeeRole === 'Manager') {
-            const manager = (data.employeeName, data.employeeEmail, data.employeeID, data.managerOfficeNumber);
-            teamDirectory.push(manager);
-            console.log(teamDirectory);
-        }
         if (data.addMoreEmployees) {
+            stepTwo(data);
             return stepOne();
         } else if (data.addMoreEmployees === false) {
+            stepTwo(data);
+            stepThree();
             console.log("team complete!");
         };
     });
+
+    function stepTwo(data) {
+        if (data.role === 'Manager') {
+            var manager = new Manager(data.name, data.email, data.id, data.officeNumber);
+            teamDirectory.push(manager);
+            console.log(teamDirectory);
+        } else if (data.role === 'Engineer') {
+            var engineer = new Engineer(data.name, data.email, data.id, data.github);
+            teamDirectory.push(engineer);
+            console.log(teamDirectory);
+        } else {
+            var intern = new Intern(data.name, data.email, data.id, data.school);
+            teamDirectory.push(intern);
+            console.log(teamDirectory);
+        };
+    };
+
+    function writeToFile(fileName, data) {
+        fs.writeFile(fileName, data, (err) => {
+            if (err) {
+                return console.log(err);
+            } else {
+                console.log("Success!")
+            }
+        })
+    }
+
+    function stepThree() {
+        writeToFile(outputPath, render(teamDirectory));
+    }
 }
 
 initialize();
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
